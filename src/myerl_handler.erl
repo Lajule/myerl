32b310bd6d28589c3f53ac72a7f0d0b9714c4ffe
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2021-09-23 17:05:03 +0200

    Add no content

--- src/myerl_handler.erl
+++ src/myerl_handler.erl
@@ -11,16 +11,11 @@ handle('POST', [<<"books">>], Req) ->
     Title = elli_request:post_arg_decoded(<<"title">>, Req, <<"undefined">>),
     Author = elli_request:post_arg_decoded(<<"author">>, Req, <<"undefined">>),
     Worker = poolboy:checkout(myerl_pool),
-    {atomic, _} =
+    ok =
         gen_server:call(Worker,
-                        {transaction,
-                         fun(Pid) ->
-                            ok =
-                                mysql:query(Pid,
-                                            <<"insert into books(title, author) values(?, ?)">>,
-                                            [Title, Author]),
-                            mysql:affected_rows(Pid)
-                         end}),
+                        {query,
+                         <<"insert into books(title, author) values(?, ?)">>,
+                         [Title, Author]}),
     poolboy:checkin(pool, Worker),
     {201, [], <<"Created">>};
 handle('GET', [<<"books">>], Req) ->
@@ -34,13 +29,16 @@ handle('GET', [<<"books">>], Req) ->
                          [Offset, Limit]}),
     poolboy:checkin(pool, Worker),
     {200, [{<<"Content-Type">>, <<"application/json">>}], jsx:encode(Rows)};
-handle('GET', [<<"books">>, BookId], Req) ->
+handle('GET', [<<"books">>, BookId], _Req) ->
     Worker = poolboy:checkout(myerl_pool),
-    {ok, _, [Row]} =
+    {ok, _, Rows} =
         gen_server:call(Worker,
                         {query, <<"select id, title, author from books where id = ?">>, [BookId]}),
     poolboy:checkin(pool, Worker),
-    {200, [{<<"Content-Type">>, <<"application/json">>}], jsx:encode(Row)};
+    case Rows of
+	[Row] -> {200, [{<<"Content-Type">>, <<"application/json">>}], jsx:encode(Row)};
+        [] -> {204, <<"No Content">>}
+    end;
 handle(_, _, _Req) ->
     {404, [], <<"Not Found">>}.
 
