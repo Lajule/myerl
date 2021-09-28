d18d8b497b7d248b256795dbed38058be59401e9
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2021-09-28 15:27:47 +0200

    Add to_map

--- src/myerl_handler.erl
+++ src/myerl_handler.erl
@@ -1,5 +1,7 @@
 -module(myerl_handler).
 
+-include_lib("kernel/include/logger.hrl").
+
 -export([handle/2, handle_event/3]).
 
 -behaviour(elli_handler).
@@ -22,22 +24,25 @@ handle('GET', [<<"books">>], Req) ->
     Offset = elli_request:get_arg(<<"offset">>, Req, <<"0">>),
     Limit = elli_request:get_arg(<<"limit">>, Req, <<"100">>),
     Worker = poolboy:checkout(myerl_pool),
-    {ok, _, Rows} =
+    {ok, ColumnNames, Rows} =
         gen_server:call(Worker,
                         {query,
                          <<"select id, title, author from books limit ?, ?">>,
                          [Offset, Limit]}),
     poolboy:checkin(pool, Worker),
-    {200, [{<<"Content-Type">>, <<"application/json">>}], jsx:encode(Rows)};
+    List = [to_map(ColumnNames, Row, #{}) || Row <- Rows],
+    {200, [{<<"Content-Type">>, <<"application/json">>}], jsx:encode(List)};
 handle('GET', [<<"books">>, BookId], _Req) ->
     Worker = poolboy:checkout(myerl_pool),
-    {ok, _, Rows} =
+    {ok, ColumnNames, Rows} =
         gen_server:call(Worker,
                         {query, <<"select id, title, author from books where id = ?">>, [BookId]}),
     poolboy:checkin(pool, Worker),
     case Rows of
         [Row] ->
-            {200, [{<<"Content-Type">>, <<"application/json">>}], jsx:encode(Row)};
+            {200,
+             [{<<"Content-Type">>, <<"application/json">>}],
+             jsx:encode(to_map(ColumnNames, Row, #{}))};
         [] ->
             {204, <<"No Content">>}
     end;
@@ -51,3 +56,8 @@ handle(_, _, _Req) ->
 
 handle_event(_Event, _Data, _Args) ->
     ok.
+
+to_map([ColumnName | ColumnNames], [Value | Values], Map) ->
+    to_map(ColumnNames, Values, maps:put(ColumnName, Value, Map));
+to_map([], [], Map) ->
+    Map.
