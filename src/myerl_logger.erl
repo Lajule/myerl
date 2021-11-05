1f1b30243e84e9e300becffb2af744171fa72188
Julien ROUZIERES <julien.rouzieres@bjtmail.com>
2021-11-05 11:48:57 +0100

    Rollback to logger

--- src/myerl_logger.erl
+++ src/myerl_logger.erl
@@ -4,6 +4,8 @@
 
 -behaviour(elli_handler).
 
+-include_lib("kernel/include/logger.hrl").
+
 handle(_Req, _Args) ->
     ignore.
 
@@ -12,13 +14,12 @@ handle_event(request_complete,
              _Args) ->
     RequestStart = proplists:get_value(request_start, Timings),
     RequestEnd = proplists:get_value(request_end, Timings),
-    lager:info("Request complete ~p",
-               [#{req => elli_request:peer(Req),
-                  time => erlang:convert_time_unit(RequestEnd - RequestStart, native, millisecond),
-                  response_code => ResponseCode,
-                  response_body => erlang:iolist_size(ResponseBody),
-                  method => elli_request:method(Req),
-                  path => elli_request:raw_path(Req)}]),
+    ?LOG_INFO(#{req => elli_request:peer(Req),
+                time => erlang:convert_time_unit(RequestEnd - RequestStart, native, millisecond),
+                response_code => ResponseCode,
+                response_body => erlang:iolist_size(ResponseBody),
+                method => elli_request:method(Req),
+                path => elli_request:raw_path(Req)}),
     ok;
 handle_event(_Event, _Data, _Args) ->
     ok.
